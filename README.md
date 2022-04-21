0f82c3dcfb38287290bd9addf4e57ef5028e8854
Simon Earman <earmansimon@gmail.com>
2022-04-21 13:04:55 +0200

    Day 4

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ### [100 Days of Code](https://www.udemy.com/course/100-days-of-code/) by Dr. Angela Yu
 
-Welcome to my repository, where I store my pieces of code for the python bootcamp.\
-I've used the mentioned bootcamp to learn python, while having some programming experience in other languages.
+Welcome to my repository, where I store pieces of code for the python bootcamp.\
+I've used the mentioned bootcamp to learn python, while having some prior programming experience in other languages.
 
 ### Projects per day
 * Day 1: [band_name.py](https://github.com/simonearman/python-bootcamp/blob/main/projects/band_name.py) (Strings and console input/output)
 * Day 2: [tip_calculator.py](https://github.com/simonearman/python-bootcamp/blob/main/projects/tip_calculator.py) (Math operations and type conversion)
 * Day 3: [treasure_island.py](https://github.com/simonearman/python-bootcamp/blob/main/projects/treasure_island.py) (Control flow and logical operators)
+* Day 4: [rock_paper_scissors.py](https://github.com/simonearman/python-bootcamp/blob/main/projects/rock_paper_scissors.py) (Random module and lists)
---
+++ projects/rock_paper_scissors.py
@@ -0,0 +1,79 @@
+from random import randint
+
+# ASCII arts by Veronica Karlsson
+rock = '''
+    _______
+---'   ____)  
+      (_____)  
+      (_____)  
+      (____)
+---.__(___)  
+'''
+
+paper = '''
+    _______
+---'   ____)____  
+          ______)  
+          _______)  
+         _______)
+---.__________)  
+'''
+
+scissors = '''
+    _______
+---'   ____)____  
+          ______)  
+       __________)  
+      (____)
+---.__(___)  
+'''
+
+print("Welcome to rock, paper, scissors")
+move = input("Type \"rock\", \"paper\" or \"scissors\" to choose your move: ").lower()
+
+# User move
+# You can use abbreviations such as "s" or "sci" to choose scissors, etc.
+if "rock".startswith(move):
+    print(f"\nYour move: rock{rock}")
+    move = "rock"
+elif "paper".startswith(move):
+    print(f"\nYour move: paper{paper}")
+    move = "rock"
+elif "scissors".startswith(move):
+    print(f"\nYour move: scissors{scissors}")
+    move = "scissors"
+else:
+    print(f"\n\"{move}\" is not an option\nTry one of the allowed moves")
+    exit()
+
+# Random AI move: 0 = rock, 1 = paper, 2 = scissors
+ai_rand = randint(0, 2)
+if ai_rand == 0:
+    print(f"AI move: rock{rock}")
+    ai_move = "rock"
+elif ai_rand == 1:
+    print(f"AI move: paper{paper}")
+    ai_move = "paper"
+else:
+    print(f"AI move: scissors{scissors}")
+    ai_move = "scissors"
+
+# Results
+# First we check for a draw
+if move == ai_move:
+    print("It's a draw!")
+elif move == "rock":
+    if ai_move == "scissors":
+        print(f"Rock beats scissors, you win!")
+    else:
+        print(f"Paper beats rock, you lose!")
+elif move == "paper":
+    if ai_move == "rock":
+        print(f"Paper beats rock, you win!")
+    else:
+        print(f"Scissors beat paper, you lose!")
+elif move == "scissors":
+    if ai_move == "paper":
+        print(f"Scissors beat paper, you win!")
+    else:
+        print(f"Rock beats scissors, you lose!")
