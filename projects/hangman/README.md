15984934d38b52a0660ad4eb5c4b1aa197dd4c45
Simon Earman <earmansimon@gmail.com>
2022-04-25 03:47:34 +0200

    Day 7 commit, hangman

---
+++ projects/hangman/README.md
@@ -0,0 +1,5 @@
+The hangman project takes all the previous chapter's knowledge (and some of my own) and combines them into a new game.\
+Sadly, my python knowledge does not (yet) extend onto editing the previously printed lines, which is why the program continuously prints everything in new lines, making a slight mess.
+
+The words.txt file has been made by Xethron (and can be modified to include some of your own words into the game)
+https://github.com/Xethron/Hangman/blob/master/words.txt
---
+++ projects/hangman/hangman.py
@@ -0,0 +1,85 @@
+import random
+import string
+
+print("Welcome to hangman\n")
+
+# Gallows image by me
+hanged_drawing = ["   ==========",
+                  "   I        |",
+                  "   I",
+                  "   I",
+                  "   I",
+                  "   I",
+                  "   I",
+                  "   I",
+                  "======="]
+
+
+# Takes a string, returns a list of characters in that string
+def split_string(input_string):
+    str_list = []
+    for char in input_string:
+        str_list.append(char)
+    return str_list
+
+
+# Takes the guessed letters, letters in the random word and player's lives left and draws the game out based on them
+def draw_game(_guessed_letters, _word_letters, _lives):
+    # Changing the hanged image based on the lives left
+    if _lives == 5:
+        hanged_drawing[2] = "   I        O"
+    if _lives == 4:
+        hanged_drawing[3] = "   I        |"
+        hanged_drawing[4] = "   I        |"
+    elif _lives == 3:
+        hanged_drawing[3] = "   I       /|"
+    elif _lives == 2:
+        hanged_drawing[3] = "   I       /|\\"
+    if _lives == 1:
+        hanged_drawing[5] = "   I       /"
+    if _lives == 0:
+        hanged_drawing[5] = "   I       / \\"
+
+    # Drawing out the word
+    word_to_draw = ""
+    for char in _word_letters:
+        if char in _guessed_letters:
+            word_to_draw += f"{char} "
+        else:
+            word_to_draw += "_ "
+    hanged_drawing[1] = f"   I        |                 CHECKED LETTERS: {', '.join(_guessed_letters)}"
+    hanged_drawing[6] = f"   I                          WORD: {word_to_draw}"
+    print("\n".join(hanged_drawing))
+
+
+# Necessary variables -> lives, reading words from words.txt and choosing a random word, and guessed letters list
+lives = 6
+words = open("words.txt").read().splitlines()
+word = random.choice(words).lower()
+word_letters = split_string(word)
+guessed_letters = []
+
+# Main gameplay code
+while True:
+    draw_game(guessed_letters, word_letters, lives)
+    if lives <= 0:
+        print(f"You didn't find the word \"{word}\" and got hanged, game over!")
+        break
+    # If all the word letters are in the guessed letters list, you win
+    elif all(l in guessed_letters for l in word_letters):
+        print("You found the word, you win!")
+        break
+
+    # Taking the user input until he enters a proper letter
+    letter = input("Which letter will you check? ").lower()
+    while len(letter) != 1 or letter not in string.ascii_lowercase:
+        letter = input(f"\"{letter}\" is not a proper single-character letter.\nGive me another letter ").lower()
+
+    # Checking if the letter is correct
+    if letter in guessed_letters:
+        lives -= 1
+    elif letter not in word_letters:
+        lives -= 1
+        guessed_letters.append(letter)
+    else:
+        guessed_letters.append(letter)
